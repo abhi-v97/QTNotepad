e2ca5459174c4d52f105dcac1af5210c250763e3
abhiv-97 <v.abhishek@outlook.com>
2021-10-10 13:35:15 +0100

    added images, functions

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -22,3 +22,100 @@ Notepad::~Notepad()
 {
     delete ui;
 }
+
+
+void Notepad::on_actionNew_triggered()
+{
+    currentFile.clear();
+    ui->textEdit->setText(QString());
+}
+
+
+void Notepad::on_actionOpen_triggered()
+{
+    QString fileName = QFileDialog::getOpenFileName(this, "Open a file");
+    QFile file(fileName);
+    currentFile = fileName;
+    if(!file.open(QIODevice::ReadOnly | QFile::Text)){
+        QMessageBox::warning(this, "Warning", "Cannot Open File: " + file.errorString());
+        return;
+    }
+    setWindowTitle(fileName);
+    QTextStream in(&file);
+    QString text = in.readAll();
+    ui->textEdit->setText(text);
+    file.close();
+}
+
+
+void Notepad::on_actionSave_As_triggered()
+{
+    QString fileName = QFileDialog::getSaveFileName(this, "Save As");
+    QFile file(fileName);
+
+    if(!file.open(QFile::WriteOnly | QFile::Text)){
+        QMessageBox::warning(this, "Warning", "Cannot Save File: " + file.errorString());
+        return;
+    }
+    currentFile = fileName;
+    setWindowTitle(fileName);
+    QTextStream out(&file);
+    QString text = ui->textEdit->toPlainText();
+    out << text;
+    file.close();
+
+}
+
+
+void Notepad::on_actionPrint_triggered()
+{
+    QPrinter printer;
+    printer.setPrinterName("Printer Name");
+    QPrintDialog pDialog(&printer, this);
+    if(pDialog.exec() == QDialog::Rejected){
+        QMessageBox::warning(this, "Warning", "Cannot Access Printer");
+        return;
+    }
+    ui->textEdit->print(&printer);
+
+}
+
+
+
+void Notepad::on_actionExit_triggered()
+{
+    QApplication::quit();
+}
+
+
+void Notepad::on_actionCopy_triggered()
+{
+    ui->textEdit->copy();
+}
+
+
+void Notepad::on_actionPaste_triggered()
+{
+    ui->textEdit->paste();
+}
+
+
+void Notepad::on_actionCut_triggered()
+{
+    ui->textEdit->cut();
+}
+
+
+
+
+void Notepad::on_actionUndo_triggered()
+{
+    ui->textEdit->undo();
+}
+
+
+void Notepad::on_actionRedo_triggered()
+{
+    ui->textEdit->redo();
+}
+
